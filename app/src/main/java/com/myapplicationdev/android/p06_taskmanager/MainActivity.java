04707dd76c29a85c2cefdbe1ba1e5fb94b403f41
KarenKuan <18055889@myrp.edu.sg>
2020-08-13 22:41:02 +0800

    P12-TaskManagerWear

--- app/src/main/java/com/myapplicationdev/android/p06_taskmanager/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p06_taskmanager/MainActivity.java
@@ -1,55 +1,99 @@
 package com.myapplicationdev.android.p06_taskmanager;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import androidx.core.app.RemoteInput;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import java.util.ArrayList;
+import androidx.core.app.NotificationCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView lv;
-    ArrayList<Task> tasks;
-    ArrayAdapter<Task> adapter;
-    Button btnAdd;
-    int actReqCode = 1;
+    int notificationId = 001; // A unique ID for our notification
+    Button btnNotif;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        lv = (ListView) findViewById(R.id.lv);
-        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnNotif = this.findViewById(R.id.btnNotif);
 
-        DBHelper dbh = new DBHelper(this);
-        tasks = dbh.getAllTasks();
-        adapter = new ArrayAdapter<Task>(this, android.R.layout.simple_list_item_1, tasks);
-        lv.setAdapter(adapter);
-
-        btnAdd.setOnClickListener(new View.OnClickListener() {
+        btnNotif.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT_WATCH)
             @Override
-            public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, AddActivity.class);
-                startActivityForResult(i, actReqCode);
+            public void onClick(View v) {
+
+                NotificationManager nm = (NotificationManager)
+                        getSystemService(Context.NOTIFICATION_SERVICE);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    NotificationChannel channel = new
+                            NotificationChannel("default", "Default Channel",
+                            NotificationManager.IMPORTANCE_DEFAULT);
+
+                    channel.setDescription("This is for default notification");
+                    nm.createNotificationChannel(channel);
+                }
+
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                NotificationCompat.Action action = new
+                        NotificationCompat.Action.Builder(
+                        R.mipmap.ic_launcher,
+                        "This is an Action",
+                        pendingIntent).build();
+
+                Intent intentReply = new Intent(MainActivity.this,
+                        ReplyActivity.class);
+                PendingIntent pendingIntentReply = PendingIntent.getActivity
+                        (MainActivity.this, 0, intentReply,
+                                PendingIntent.FLAG_UPDATE_CURRENT);
+
+                RemoteInput ri = new RemoteInput.Builder("status")
+                        .setLabel("Status report")
+                        .setChoices(new String [] {"Done", "Not yet"})
+                        .build();
+
+                NotificationCompat.Action action2 = new
+                        NotificationCompat.Action.Builder(
+                        R.mipmap.ic_launcher,
+                        "Reply",
+                        pendingIntentReply)
+                        .addRemoteInput(ri)
+                        .build();
+
+                NotificationCompat.WearableExtender extender = new
+                        NotificationCompat.WearableExtender();
+                extender.addAction(action);
+                extender.addAction(action2);
+
+                String text = getString(R.string.basic_notify_msg);
+                String title = getString(R.string.notification_title);
+
+                NotificationCompat.Builder builder = new
+                        NotificationCompat.Builder(MainActivity.this, "default");
+                builder.setContentText(text);
+                builder.setContentTitle(title);
+                builder.setSmallIcon(android.R.drawable.btn_star_big_off);
+
+                // Attach the action for Wear notification created above
+                builder.extend(extender);
+
+                Notification notification = builder.build();
+
+                nm.notify(notificationId, notification);
             }
         });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == actReqCode) {
-            if (resultCode == RESULT_OK) {
-                DBHelper dbh = new DBHelper(MainActivity.this);
-                tasks.clear();
-                tasks.addAll(dbh.getAllTasks());
-                dbh.close();
-                adapter.notifyDataSetChanged();
-            }
-        }
-        super.onActivityResult(requestCode, resultCode, data);
     }
 }
